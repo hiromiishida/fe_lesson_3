dbe4ff5f02c5a986830f33ce5a77697429c591e8
digitalplan <digitalplan21@gmail.com>
2022-10-28 10:25:58 +0900

    クラス追加

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 3;
+        int result = num1 % num2;
+
+        System.out.println(result);
+    }
+}
